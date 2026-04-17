12a48d8701aab12078c802ad85076c1853a7f0e8
KEERTHIK T <140291659+Keerthik-T@users.noreply.github.com>
2026-04-17 20:09:13 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
                 <h3 className="text-4xl md:text-6xl font-bold text-slate-600 dark:text-slate-400 mb-8 transition-colors">I build experiences for the web.</h3>
                 <p className="text-slate-600 dark:text-slate-400 text-lg md:text-xl max-w-2xl mx-auto mb-12 transition-colors">
                     
-                </p>"With a core foundation in Linux environments and Computer Networking, I specialize in developing secure, scalable digital solutions. My methodology is rooted in Social Engineering and Analytical Psychology; I utilize cognitive modeling to understand how users interact with security protocols. This unique perspective allows me to engineer systems that prioritize robust defense without sacrificing intuitive human experience
+                </p> With a core foundation in Linux environments and Computer Networking, I specialize in developing secure, scalable digital solutions. My methodology is rooted in Social Engineering and Analytical Psychology; I utilize cognitive modeling to understand how users interact with security protocols.
                 <div className="flex justify-center gap-6 flex-wrap">
                     <a href="https://www.linkedin.com/in/keerthik-t-0b73bb36a/" target="_blank" className="px-8 py-4 bg-primary text-white rounded-lg font-medium hover:bg-indigo-600 transition-colors shadow-lg shadow-indigo-500/30 flex items-center gap-2">
                         <i className="ph ph-linkedin-logo text-2xl"></i> Connect on LinkedIn
